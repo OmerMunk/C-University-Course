c07ff761c4c04caf83551a0ed0ae83eb57820061
Omer Munk <93129669+OmerMunk@users.noreply.github.com>
2022-04-14 22:18:48 +0300

    Add files via upload

---
+++ Task1/Exercise1/header.h
@@ -0,0 +1,14 @@
+
+/* Decleration of the scalar product function  */
+int scalar_product(int n, int vec1[], int vec2[]);
+
+/* Decleration of the scalar product driver function  */
+int scalar_product_driver();
+
+/* Decleration of the scalar product user input function  */
+int scalar_product_run_user_input();
+
+/* Decleration of the scalar product file input function  */
+int scalar_product_run_file();
+
+#define MAX_SIZE 100
